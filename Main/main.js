b9f9921c437d53b105bca001f959f7c142aebb53
TiamatBronze1 <95623386+TiamatBronze1@users.noreply.github.com>
2024-01-24 18:43:20 +0300

    new

--- Main/main.js
+++ Main/main.js
@@ -1,17 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const startAdventureButton = document.getElementById('startAdventureButton');
-    const mirageSection = document.querySelector('.mirage-section');
-
-    startAdventureButton.addEventListener('click', function () {
-        mirageSection.style.animation = 'fadeOut 1s ease';
-        setTimeout(function () {
-            mirageSection.style.display = 'none';
-        }, 1000);
-
-        // Прокрутка страницы к следующему разделу (замените '#nextSection' на селектор вашего следующего раздела)
-        window.scrollTo({
-            top: document.querySelector('#nextSection').offsetTop,
-            behavior: 'smooth'
-        });
-    });
+document.querySelector('.right-arrow').addEventListener('click', () => {
+    const cards = document.querySelectorAll('.server-card');
+    const firstCard = cards[0];
+    firstCard.parentElement.appendChild(firstCard);
+    updateCardStyles();
 });
+
+document.querySelector('.left-arrow').addEventListener('click', () => {
+    const cards = document.querySelectorAll('.server-card');
+    const lastCard = cards[cards.length - 1];
+    lastCard.parentElement.prepend(lastCard);
+    updateCardStyles();
+});
+
+function updateCardStyles() {
+    const cards = document.querySelectorAll('.server-card');
+    cards.forEach(card => card.classList.remove('greyed-out'));
+    if (cards.length > 2) {
+        cards[0].classList.add('greyed-out');
+        cards[cards.length - 1].classList.add('greyed-out');
+    }
+}
+
+
+document.querySelector('.start-button').addEventListener('click', () => {
+    const serversSection = document.querySelector('.servers');
+    serversSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+updateCardStyles(); 
